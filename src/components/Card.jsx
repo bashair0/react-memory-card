d1c6e576c0a9dfed8ff8a99612dc71e4d2e30230
bashair0 <bisho5.ba@gmail.com>
2024-11-28 11:27:01 +0800

    Modify the url name

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import "../styles/Card.css";
 
-export default function Card({ pokeURL }) {
+export default function Card({ pokeURL, index, handleCardClick }) {
   const [pokemonData, setPokemonData] = useState({});
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [pokeURL]);
 
   async function fetchData() {
     const response = await fetch(pokeURL);
@@ -14,14 +14,12 @@ export default function Card({ pokeURL }) {
   }
 
   return (
-    <>
-      <div className="Card">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonData.id}.svg`}
-          alt={pokemonData.name}
-        />
-        <p>{pokemonData.name}</p>
-      </div>
-    </>
+    <div className="Card" onClick={() => handleCardClick(index)}>
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonData.id}.svg`}
+        alt={pokemonData.name}
+      />
+      <p>{pokemonData.name}</p>
+    </div>
   );
 }
